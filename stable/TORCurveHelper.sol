620cbc521a0421dc070575c59f8aba6718248296
Hector DAO <93325815+HectorDAO-HEC@users.noreply.github.com>
2022-02-06 01:43:59 +0800

    Update TORCurveHelper.sol

--- stable/TORCurveHelper.sol
+++ stable/TORCurveHelper.sol
@@ -73,18 +73,19 @@ interface ITORCurveHelper{
 }
 contract TORCurveHelper is ITORCurveHelper{
     using SafeMath for uint;
-    function getTorAndStable() view public returns(uint torAmount,uint stableAmount){//1e18 for torAmount and stableAmount
+    function getTorAndDaiAndUsdc() view public returns(uint torAmount,uint daiAmount,uint usdcAmount){//1e18 for torAmount and stableAmount
         torAmount=ICurve(0x24699312CB27C26Cfc669459D670559E5E44EE60).balances(0);
         uint c2Amount=ICurve(0x24699312CB27C26Cfc669459D670559E5E44EE60).balances(1);
         ICurve c2pool=ICurve(0x27E611FD27b276ACbd5Ffd632E5eAEBEC9761E40);
-        stableAmount=c2pool.balances(1).mul(1e12).mul(c2Amount).div(c2pool.totalSupply()).add(c2pool.balances(0).mul(c2Amount).div(c2pool.totalSupply()));
+        usdcAmount=c2pool.balances(1).mul(1e12).mul(c2Amount).div(c2pool.totalSupply());
+        daiAmount=c2pool.balances(0).mul(c2Amount).div(c2pool.totalSupply());
     }
     function getPoolPercentageWithMint(uint _torAmount) override view external returns(uint percentage){//tor percentage 10000 = 100%
-        (uint torAmount,uint stableAmount)=getTorAndStable();
-        percentage=torAmount.add(_torAmount).mul(1e4).div(torAmount.add(stableAmount));
+        (uint torAmount,uint daiAmount,uint usdcAmount)=getTorAndDaiAndUsdc();
+        percentage=torAmount.add(_torAmount).mul(1e4).div(torAmount.add(daiAmount).add(usdcAmount));
     }
     function getPoolPercentageWithRedeem(uint _torAmount) override view external returns(uint percentage){//tor percentage 5000 = 50%
-        (uint torAmount,uint stableAmount)=getTorAndStable();
-        percentage=torAmount.sub(_torAmount).mul(1e4).div(torAmount.add(stableAmount));
+        (uint torAmount,uint daiAmount,uint usdcAmount)=getTorAndDaiAndUsdc();
+        percentage=torAmount.sub(_torAmount).mul(1e4).div(torAmount.add(daiAmount).add(usdcAmount));
     }
 }
