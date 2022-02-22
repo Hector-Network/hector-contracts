c8eaeb4c65c8cf24e6a2c69febfb5bf69bb9f55d
Hector DAO <93325815+HectorDAO-HEC@users.noreply.github.com>
2022-02-23 00:58:31 +0800

    Create TorLpPricer.sol

---
+++ stable/farm/TorLpPricer.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+
+    function sqrrt(uint256 a) internal pure returns (uint c) {
+        if (a > 3) {
+            c = a;
+            uint b = add( div( a, 2), 1 );
+            while (b < c) {
+                c = b;
+                b = div( add( div( a, b ), b), 2 );
+            }
+        } else if (a != 0) {
+            c = 1;
+        }
+    }
+}
+interface IERC20 {
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+interface IPricer{
+    //price for 1 token unit (10^tokenDecimals), price is in 1e18
+    function price(IERC20 token) external view returns(uint);
+}
+interface IUniswapV2Pair {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function token0() external view returns ( address );
+    function token1() external view returns ( address );
+    function totalSupply() external view returns (uint);
+    function decimals() external view returns (uint8);
+}
+contract TorLpPricer is IPricer{
+    using SafeMath for uint;
+    function price(IERC20 token) external view override returns(uint){
+        IUniswapV2Pair pair = IUniswapV2Pair(address(token));
+        require(pair.token0()==0x74E23dF9110Aa9eA0b6ff2fAEE01e740CA1c642e||pair.token1()==0x74E23dF9110Aa9eA0b6ff2fAEE01e740CA1c642e,"only tor uni pair is supported");
+        (uint reserve0, uint reserve1, ) = pair.getReserves();
+        uint totalTorAmount=reserve0;
+        if(pair.token1()==0x74E23dF9110Aa9eA0b6ff2fAEE01e740CA1c642e){
+            totalTorAmount=reserve1;
+        }
+        return totalTorAmount.mul(2).mul(10^pair.decimals()).div(pair.totalSupply());
+    }
+}
