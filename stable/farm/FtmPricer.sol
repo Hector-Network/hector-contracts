33594f8624daf0d7b91a86f83e8cc89a0289f81e
Hector DAO <93325815+HectorDAO-HEC@users.noreply.github.com>
2022-02-22 22:02:03 +0800

    Create FtmPricer.sol

---
+++ stable/farm/FtmPricer.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+
+    function sqrrt(uint256 a) internal pure returns (uint c) {
+        if (a > 3) {
+            c = a;
+            uint b = add( div( a, 2), 1 );
+            while (b < c) {
+                c = b;
+                b = div( add( div( a, b ), b), 2 );
+            }
+        } else if (a != 0) {
+            c = 1;
+        }
+    }
+}
+interface AggregatorV3Interface {
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+}
+interface IERC20 {
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+interface IPricer{
+    //price for 1 token unit (10^tokenDecimals), price is in 1e18
+    function price(IERC20 token) external view returns(uint);
+}
+contract FtmPricer is IPricer{
+    using SafeMath for uint;
+    function price(IERC20 token) external view override returns(uint){
+        require(address(token)==0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83,"only for wftm");
+        ( , int _price, , , ) = AggregatorV3Interface(0xf4766552D15AE4d256Ad41B6cf2933482B0680dc).latestRoundData();
+        return uint(_price).mul(1e10);
+    }
+    function convertDecimal(uint8 fromDec,uint8 toDec, uint fromAmount) pure public returns(uint toAmount){
+        if(fromDec==toDec) toAmount=fromAmount;
+        else if(fromDec>toDec) toAmount=fromAmount.div(10**(fromDec-toDec));
+        else toAmount=fromAmount.mul(10**(toDec-fromDec));
+    }
+    function e18(uint8 fromDec,uint fromAmount) pure public returns(uint toAmount){
+        return convertDecimal(fromDec,18,fromAmount);
+    }
+}
