e3252bc50e3cf8d55254270086dff697b3e9d4b0
Hector Finance <93325815+HectorDAO-HEC@users.noreply.github.com>
2022-03-31 15:01:13 +0800

    Update RewardReceiver.sol

--- emission/RewardReceiver.sol
+++ emission/RewardReceiver.sol
@@ -56,6 +56,7 @@ contract Ownable is IOwnable {
 }
 interface IERC20{
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
 }
 interface IRewardReceiver{
