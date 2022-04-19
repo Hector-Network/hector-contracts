e38e86b0c4c40a8bc08b541d408d32b93920d123
0x A.K <0x.bejaxer@gmail.com>
2022-04-20 00:23:43 +1000

    update: emissionor

---
+++ lock-farm/interfaces/IEmissionor.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.7;
+
+interface IEmissionor {
+    function getBeginTime() external view returns (uint256);
+
+    function getEndTime() external view returns (uint256);
+}
