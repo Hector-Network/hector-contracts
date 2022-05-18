b6c33d2f621c0bda52e73609926f0680b17e3621
Hector Finance <93325815+HectorDAO-HEC@users.noreply.github.com>
2022-05-18 22:10:35 +0800

    Update MockTreasury.sol

--- stable/MockTreasury.sol
+++ stable/MockTreasury.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.7.5;
+pragma solidity 0.8.7;
 interface ITreasury{
     function mintRewards( address _recipient, uint _amount ) external;
+    function manage( address _token, uint _amount ) external;
 }
 interface IERC20{
     function mint(address account_, uint256 amount_) external;
+    function transfer(address recipient, uint256 amount) external returns (bool);
 }
 contract MockTreasury is ITreasury{
     modifier onlyOwner() {
@@ -14,6 +16,7 @@ contract MockTreasury is ITreasury{
     address public _owner;
     address public minter;
     address public hec;
+    address public reserveManager;
     constructor(){
         _owner=msg.sender;
     }
@@ -21,10 +24,17 @@ contract MockTreasury is ITreasury{
         hec=_hec;
     }
     function mintRewards( address _recipient, uint _amount ) override external{
-        require(msg.sender==minter);
+        require(msg.sender==minter,"only minter can mint");
         IERC20(hec).mint(_recipient,_amount);
+    }
+    function manage( address _token, uint _amount ) override external {
+        require(msg.sender==reserveManager,"only manager can manage");
+        IERC20(_token).transfer(msg.sender,_amount);
     }
     function setMinter(address _minter) external onlyOwner(){
         minter=_minter;
+    }
+    function setReserveManager(address _reserveManager) external onlyOwner(){
+        reserveManager=_reserveManager;
     }
 }
