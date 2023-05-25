d46d1106fec4aa4c0006602e26d4cff99c2fb99a
0x A.K <0x.bejaxer@gmail.com>
2023-05-25 12:32:11 +1000

    fix: External contracts

---
+++ pay/contracts/external/uniswapV2/IUniswapV2Router.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.9;
+
+interface IUniswapV2Router {
+    function factory() external pure returns (address);
+
+    // solhint-disable-next-line func-name-mixedcase
+    function WETH() external view returns (address);
+
+    function getAmountsIn(uint256 amountOut, address[] memory path)
+        external
+        view
+        returns (uint256[] memory amounts);
+
+    function getAmountsOut(uint256 amountIn, address[] memory path)
+        external
+        view
+        returns (uint256[] memory amounts);
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+}
