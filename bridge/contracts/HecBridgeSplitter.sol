508aa98e59b2d80a260c23ab1a2bdff4a7ecac84
Michael Nguyen <mikedallas10@gmail.com>
2023-06-03 14:27:23 -0500

    Fixed vulnerability from DAO Hacking

--- bridge/contracts/HecBridgeSplitter.sol
+++ bridge/contracts/HecBridgeSplitter.sol
@@ -5,78 +5,22 @@ import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol';
+import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 
-interface IOwnableUpgradeable {
-	function owner() external view returns (address);
-
-	function renounceManagement() external;
-
-	function pushManagement(address newOwner_) external;
-
-	function pullManagement() external;
-}
-
-abstract contract OwnableUpgradeable is IOwnableUpgradeable, Initializable, ContextUpgradeable {
-	address internal _owner;
-	address internal _newOwner;
-
-	event OwnershipPushed(address indexed previousOwner, address indexed newOwner);
-	event OwnershipPulled(address indexed previousOwner, address indexed newOwner);
-
-	/**
-	 * @dev Initializes the contract setting the deployer as the initial owner.
-	 */
-	function __Ownable_init() internal onlyInitializing {
-		__Ownable_init_unchained();
-	}
-
-	function __Ownable_init_unchained() internal onlyInitializing {
-		_owner = msg.sender;
-		emit OwnershipPushed(address(0), _owner);
-	}
-
-	function owner() public view override returns (address) {
-		return _owner;
-	}
-
-	modifier onlyOwner() {
-		require(_owner == msg.sender, 'Ownable: caller is not the owner');
-		_;
-	}
-
-	function renounceManagement() public virtual override onlyOwner {
-		emit OwnershipPushed(_owner, address(0));
-		_owner = address(0);
-	}
-
-	function pushManagement(address newOwner_) public virtual override {
-		require(newOwner_ != address(0), 'Ownable: new owner is the zero address');
-		emit OwnershipPushed(_owner, newOwner_);
-		_newOwner = newOwner_;
-	}
-
-	function pullManagement() public virtual override {
-		require(msg.sender == _newOwner, 'Ownable: must be new owner to pull');
-		emit OwnershipPulled(_owner, _newOwner);
-		_owner = _newOwner;
-	}
-
-	/**
-	 * @dev This empty reserved space is put in place to allow future versions to add new
-	 * variables without shifting down storage in the inheritance chain.
-	 * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-	 */
-	uint256[49] private __gap;
-}
+error INVALID_PARAM();
+error INVALID_ADDRESS();
+error INVALID_AMOUNT();
+error INVALID_ALLOWANCE();
+error BRIDGE_FAILED();
 
 /**
  * @title HecBridgeSplitter
  */
-contract HecBridgeSplitter is OwnableUpgradeable, PausableUpgradeable {
+contract HecBridgeSplitterV2 is OwnableUpgradeable, PausableUpgradeable {
 	using SafeMathUpgradeable for uint256;
 	using SafeERC20Upgradeable for IERC20Upgradeable;
 
-	address public LiFiBridge;
+	address public BridgeContract;
 	uint256 public CountDest; // Count of the destination wallets
 
 	// Struct Asset Info
@@ -96,9 +40,10 @@ contract HecBridgeSplitter is OwnableUpgradeable, PausableUpgradeable {
 	 * @dev sets initials
 	 */
 	function initialize(uint256 _CountDest, address _bridge) external initializer {
-		LiFiBridge = _bridge;
+		if (_bridge == address(0)) revert INVALID_ADDRESS();
+		if (_CountDest == 0) revert INVALID_PARAM();
+		BridgeContract = _bridge;
 		CountDest = _CountDest;
-		__Ownable_init();
 		__Pausable_init();
 	}
 
@@ -110,45 +55,40 @@ contract HecBridgeSplitter is OwnableUpgradeable, PausableUpgradeable {
 	/// @param sendingAssetInfos Array Data used purely for sending assets
 	/// @param fees Amounts of native coin amounts for bridge
 	/// @param callDatas CallDatas from lifi sdk
-	/// @param useSquid use Squid or Lifi
-	/// @param squidTargetAddress use in executing squid bridge contract
 	function Bridge(
 		SendingAssetInfo[] memory sendingAssetInfos,
 		uint256[] memory fees,
-		bytes[] memory callDatas,
-		bool useSquid,
-		address squidTargetAddress
+		bytes[] memory callDatas
 	) external payable {
-		require(
+		if (
 			sendingAssetInfos.length > 0 &&
 				sendingAssetInfos.length <= CountDest &&
-				sendingAssetInfos.length == callDatas.length,
-			'Splitter: bridge or swap call data is invalid'
-		);
-		require(useSquid && squidTargetAddress != address(0), 'Splitter: squid router is invalid');
+				sendingAssetInfos.length == callDatas.length)
+			revert INVALID_PARAM();		
 
-		address callTargetAddress = useSquid ? squidTargetAddress : LiFiBridge;
+		bool isFeeEnabled = msg.value > 0 && fees.length > 0;
+		
 		for (uint256 i = 0; i < sendingAssetInfos.length; i++) {
-			if (sendingAssetInfos[i].sendingAssetId != address(0)) {
-				IERC20Upgradeable srcToken = IERC20Upgradeable(sendingAssetInfos[i].sendingAssetId);
+			address sendingAssetId = sendingAssetInfos[i].sendingAssetId;
+			if (sendingAssetId == address(0)) revert INVALID_ADDRESS();
+			bytes memory callData = callDatas[i];
+			
+			IERC20Upgradeable srcToken = IERC20Upgradeable(sendingAssetId);
 
-				require(
-					srcToken.allowance(msg.sender, address(this)) > 0,
-					'ERC20: transfer amount exceeds allowance'
-				);
+			if (srcToken.allowance(msg.sender, address(this)) == 0) revert INVALID_ALLOWANCE();
 
-				srcToken.safeTransferFrom(msg.sender, address(this), sendingAssetInfos[i].sendingAmount);
-				srcToken.approve(callTargetAddress, sendingAssetInfos[i].sendingAmount);
-			}
+			uint256 srcAmount = sendingAssetInfos[i].sendingAmount;
+			srcToken.safeTransferFrom(msg.sender, address(this), srcAmount);
+			srcToken.approve(BridgeContract, srcAmount);
 
-			if (msg.value > 0 && fees.length > 0 && fees[i] > 0) {
-				(bool success, ) = payable(callTargetAddress).call{value: fees[i]}(callDatas[i]);
-				require(success, 'Splitter: bridge swap transaction was failed');
-				emit CallData(success, callDatas[i]);
+			if (isFeeEnabled && fees[i] > 0) {
+				(bool success, ) = payable(BridgeContract).call{value: fees[i]}(callData);
+				if (!success) revert BRIDGE_FAILED();
+				emit MakeCallData(success, callData, msg.sender);
 			} else {
-				(bool success, ) = payable(callTargetAddress).call(callDatas[i]);
-				require(success, 'Splitter: bridge swap transaction was failed');
-				emit CallData(success, callDatas[i]);
+				(bool success, ) = payable(BridgeContract).call(callData);
+				if (!success) revert BRIDGE_FAILED();
+				emit MakeCallData(success, callData, msg.sender);
 			}
 		}
 
@@ -157,19 +97,30 @@ contract HecBridgeSplitter is OwnableUpgradeable, PausableUpgradeable {
 
 	// Custom counts of detinations
 	function setCountDest(uint256 _countDest) external onlyOwner {
+		if (_countDest == 0) revert INVALID_PARAM();
+		uint256 oldCountDest = CountDest;
 		CountDest = _countDest;
-		emit SetCountDest(_countDest);
+		emit SetCountDest(oldCountDest, _countDest, msg.sender);
 	}
 
-	// Set LiFiDiamond Address
 	function setBridge(address _bridge) external onlyOwner {
-		LiFiBridge = _bridge;
-		emit SetBridge(_bridge);
+		if (_bridge == address(0)) revert INVALID_ADDRESS();
+		//check if _bridge is a contract not wallet
+		uint256 size;
+		assembly {
+			size := extcodesize(_bridge)
+		}
+		if (size == 0) revert INVALID_ADDRESS();
+
+		address oldBridge = BridgeContract;
+
+		BridgeContract = _bridge;
+		emit SetBridge(oldBridge, _bridge, msg.sender);
 	}
 
 	// All events
-	event SetCountDest(uint256 countDest);
-	event SetBridge(address bridge);
-	event CallData(bool success, bytes callData);
-	event HectorBridge(address user, SendingAssetInfo[] sendingAssetInfos);
+	event SetCountDest(uint256 oldCountDest, uint256 newCountDest, address indexed user);
+	event SetBridge(address oldBridge, address newBridge, address indexed user);
+	event MakeCallData(bool success, bytes callData, address indexed user);
+	event HectorBridge(address indexed user, SendingAssetInfo[] sendingAssetInfos);
 }
