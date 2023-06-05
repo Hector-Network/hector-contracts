c979da6aa701edfcca4b2300b87011095b973ec9
xiangdev1016 <xiangchu1016@gmail.com>
2023-06-05 09:37:17 -0400

    update bridge

--- bridge/contracts/HecBridgeSplitter.sol
+++ bridge/contracts/HecBridgeSplitter.sol
@@ -12,6 +12,7 @@ error INVALID_ADDRESS();
 error INVALID_AMOUNT();
 error INVALID_ALLOWANCE();
 error INVALID_PERCENTAGE();
+error INVALID_FEES();
 error DAO_FEE_FAILED();
 
 /**
@@ -75,29 +76,28 @@ contract HecBridgeSplitter is OwnableUpgradeable, PausableUpgradeable {
 			isCallAddress[callTargetAddress]
 		) revert INVALID_PARAM();
 
+		if (msg.value < sum(fees)) revert INVALID_FEES();
+
 		uint256 length = sendingAssetInfos.length;
 		for (uint256 i = 0; i < length; i++) {
-			if (sendingAssetInfos[i].feePercentage < minFeePercentage) revert INVALID_PERCENTAGE();
-			if (sendingAssetInfos[i].sendingAssetId != address(0)) {
-				IERC20Upgradeable srcToken = IERC20Upgradeable(sendingAssetInfos[i].sendingAssetId);
+			SendingAssetInfo memory sendingAssetInfo = sendingAssetInfos[i];
 
-				if (srcToken.allowance(msg.sender, address(this)) == 0) revert INVALID_ALLOWANCE();
-				if (srcToken.balanceOf(msg.sender) < sendingAssetInfos[i].totalAmount)
-					revert INVALID_AMOUNT();
-
-				uint256 calcBridgeAmount = sendingAssetInfos[i].sendingAmount;
+			if (sendingAssetInfo.feePercentage < minFeePercentage) revert INVALID_PERCENTAGE();
+			if (sendingAssetInfo.sendingAssetId != address(0)) {
+				IERC20Upgradeable srcToken = IERC20Upgradeable(sendingAssetInfo.sendingAssetId);
 
 				uint256 beforeBalance = srcToken.balanceOf(address(this));
-				srcToken.safeTransferFrom(msg.sender, address(this), sendingAssetInfos[i].totalAmount);
+				srcToken.safeTransferFrom(msg.sender, address(this), sendingAssetInfo.totalAmount);
 				uint256 afterBalance = srcToken.balanceOf(address(this));
-				if (afterBalance - beforeBalance != sendingAssetInfos[i].totalAmount)
+				if (afterBalance - beforeBalance != sendingAssetInfo.totalAmount)
 					revert INVALID_AMOUNT();
 
+				uint256 calcBridgeAmount = sendingAssetInfo.sendingAmount;
 				srcToken.approve(callTargetAddress, calcBridgeAmount);
 			}
 			bytes memory callData = callDatas[i];
 
-			if (msg.value > sum(fees) && msg.value > 0 && fees.length > 0 && fees[i] > 0) {
+			if (msg.value > 0 && fees.length > 0 && fees[i] > 0) {
 				(bool success, bytes memory result) = payable(callTargetAddress).call{value: fees[i]}(
 					callData
 				);
@@ -108,20 +108,20 @@ contract HecBridgeSplitter is OwnableUpgradeable, PausableUpgradeable {
 				if (!success) revert(_getRevertMsg(result));
 				emit MakeCallData(success, callData, msg.sender);
 			}
-			_takeFee(sendingAssetInfos[i]);
+			_takeFee(sendingAssetInfo);
 		}
 
 		emit HectorBridge(msg.sender, sendingAssetInfos);
 	}
 
 	// Sum
-	function sum(uint256[] memory numbers) public pure returns (uint256) {
-        uint256 total = 0;
-        for (uint i = 0; i < numbers.length; i++) {
-            total += numbers[i];
-        }
-        return total;
-    }
+	function sum(uint256[] memory numbers) internal pure returns (uint256) {
+		uint256 total = 0;
+		for (uint i = 0; i < numbers.length; i++) {
+			total += numbers[i];
+		}
+		return total;
+	}
 
 	// Return revert msg of failed Bridge transaction
 	function _getRevertMsg(bytes memory _returnData) internal pure returns (string memory) {
