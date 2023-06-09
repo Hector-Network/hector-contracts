35bc1647b3a382f15f7df33d6b5985aebc68b05d
xiangdev1016 <xiangchu1016@gmail.com>
2023-06-08 22:33:30 -0400

    update test case

--- bridge/test/bridge.squid.test.ts
+++ bridge/test/bridge.squid.test.ts
@@ -283,7 +283,6 @@ describe('Hector Bridge', function () {
 					targetAddress,
 					{
 						value: fee,
-						gasLimit: 1000000
 					}
 				);
 				await expect(result.wait()).not.to.be.reverted;
